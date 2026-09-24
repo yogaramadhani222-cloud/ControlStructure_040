a578851b01782c28f98c0a4093fb46b4be9a1ea4
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-09-24 10:56:52 +0700

    menentukan performa pelajar

---
+++ ControlStructure_040.py
@@ -0,0 +1,10 @@
+performance = input("Please enter your performance")
+
+if performance >= "90":
+    print("Excellent")
+elif performance >= "80":
+    print("Very Good Performance")
+elif performance >= "70":
+    print("Good Performance")
+elif performance >= "60":
+    print("Then Average Performance")
